1be115f1f24292c47e74ce35d781f77e51321b22
achilezweb@gmail.com <achilezweb@gmail.com>
2024-08-24 19:26:06 +0800

    added 20

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -175,10 +175,10 @@ function handleImageError() {
                             </div>
 
                             <div class="pt-3 sm:pt-5">
-                                <h2 class="text-xl font-semibold text-black dark:text-white">Laracast19</h2>
+                                <h2 class="text-xl font-semibold text-black dark:text-white">Laracast20</h2>
 
                                 <p class="mt-4 text-sm/relaxed">
-                                    Laracasts19 offers thousands of video tutorials on Laravel, PHP, and JavaScript
+                                    Laracasts20 offers thousands of video tutorials on Laravel, PHP, and JavaScript
                                     development. Check them out, see for yourself, and massively level up your
                                     development skills in the process.
                                 </p>
